bdb6d2a4b35968798d2140534f77ddf84779e966
MetaBogdan <63185002+MetaBogdan@users.noreply.github.com>
2025-12-02 19:23:33 -0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Pr from "./Pr.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Pr from './Pr.jsx'; //
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Pr />
   </React.StrictMode>
